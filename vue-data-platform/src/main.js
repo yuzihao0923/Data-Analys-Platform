e439c45977f91164d2c2e5268d503910bbbaa96d
yuzihao0923 <a00278220@student.ait.ie>
2024-09-27 10:28:10 +0800

    successfully connect mysql server

--- vue-data-platform/src/main.js
+++ vue-data-platform/src/main.js
@@ -1,7 +1,10 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router'; // 引入路由器
+import Axios from 'axios'
 
 createApp(App)
   .use(router) // 使用路由器
   .mount('#app'); // 挂载到 #app
+
+Vue.prototype.$axios = Axios
