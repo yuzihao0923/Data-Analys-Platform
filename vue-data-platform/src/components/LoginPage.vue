7d3f48dcceed214850771fbaaedbb317a1273e69
yuzihao0923 <a00278220@student.ait.ie>
2024-09-30 10:14:43 +0800

    create login page structure: use login page to access to different page for different roles

---
+++ vue-data-platform/src/components/LoginPage.vue
@@ -0,0 +1,19 @@
+<template>
+    <div id="loginTitle">
+        <h1>Use username & password to login</h1>
+    </div>
+
+    <div>
+    
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
